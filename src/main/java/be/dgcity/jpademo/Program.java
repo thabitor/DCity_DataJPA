33eb8512bf077e93eacbbb3bcfae9024e0ce2d7c
thabitor <thabithamror@gmail.com>
2022-07-05 11:10:15 +0200

    Entity manager added

--- src/main/java/be/dgcity/jpademo/Program.java
+++ src/main/java/be/dgcity/jpademo/Program.java
@@ -1,5 +1,6 @@
 package be.dgcity.jpademo;
 
+import be.dgcity.jpademo.entities.Section;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -11,8 +12,10 @@ public class Program {
         EntityManagerFactory emf =
                 Persistence.createEntityManagerFactory("PersistenceDemoMySQL");
         EntityManager manager = emf.createEntityManager();
+        Section s = manager.find(Section.class, 1010);
 
-        
+
+
 
         emf.close();
 
